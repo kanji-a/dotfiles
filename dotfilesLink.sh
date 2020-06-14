0b653fb28b1be80a66d45f9a5079c87f16c709fe
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2020-06-15 00:20:12 +0900

    oh-my-zshを使うので自前のzshrc使わなくなった

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/colors ~/.vim
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/git/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/git/dotfiles/.gvimrc ~/.gvimrc
+ln -sf ~/git/dotfiles/.bashrc ~/.bashrc
