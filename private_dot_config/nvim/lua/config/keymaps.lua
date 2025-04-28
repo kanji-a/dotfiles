eabda30f88541cd3c7d9b1b5f6f2a9cb830c5e5e
kanji-a <qazsedcdeszaq@gmail.com>
2025-04-28 17:31:17 +0900

    キーマップ移植

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+map('n', 'j', 'gj', opts)
+map('n', 'k', 'gk', opts)
+map('n', 'Y', 'y$', opts)
+map('n', '<Esc><Esc>', ':nohlsearch<CR><Esc>', opts)
