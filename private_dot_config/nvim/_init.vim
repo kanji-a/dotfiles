81f0b27282dd2ab006c94d422d186d7d1a8019a0
kanji-a <qazsedcdeszaq@gmail.com>
2025-04-28 18:57:08 +0900

    オプション移植

--- private_dot_config/nvim/_init.vim
+++ private_dot_config/nvim/_init.vim
@@ -11,20 +11,10 @@ set autoread
 set hidden
 " 入力中のコマンドをステータスに表示する
 set showcmd
-" クリップボードを使えるようにする
-set clipboard+=unnamedplus
 
 " 見た目系
-" 行番号を表示
-set number
-" 現在の行を強調表示
-set cursorline
 " 現在の行を強調表示（縦）
 " set cursorcolumn
-" 行末の1文字先までカーソルを移動できるように
-set virtualedit=onemore
-" インデントはスマートインデント
-set smartindent
 " ビープ音を可視化
 set visualbell
 " 括弧入力時の対応する括弧を表示
@@ -43,19 +33,9 @@ syntax enable
 " Tab系
 " 不可視文字を可視化(タブが「▸-」と表示される)
 set list listchars=tab:\▸\-
-" Tab文字を半角スペースにする
-set expandtab
-" 行頭以外のTab文字の表示幅（スペースいくつ分）
-set tabstop=2
-" 行頭でのTab文字の表示幅
-set shiftwidth=2
 
 
 " 検索系
-" 検索文字列が小文字の場合は大文字小文字を区別なく検索する
-set ignorecase
-" 検索文字列に大文字が含まれている場合は区別して検索する
-set smartcase
 " 検索文字列入力時に順次対象文字列にヒットさせる
 set incsearch
 " 検索時に最後まで行ったら最初に戻る
--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,27 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+opt.fileencoding = "utf-8"
+opt.backup = false
+opt.swapfile = false
+opt.autoread = true
+opt.hidden = true
+opt.showcmd = true
+opt.clipboard = "unnamedplus"
+opt.number = true
+opt.cursorline = true
+opt.virtualedit = "onemore"
+opt.smartindent = true
+opt.visualbell = true
+opt.showmatch = true
+opt.laststatus = 2
+opt.wildmode = "list:longest"
+opt.syntax = "enable"
+opt.list = true
+opt.listchars = "tab:▸-"
+opt.incsearch = true
+opt.wrapscan = true
+opt.hlsearch = true
