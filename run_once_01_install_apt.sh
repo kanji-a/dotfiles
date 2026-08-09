38149d31d4370f87eed58a814e5a7012c4a1b191
kanji-a <qazsedcdeszaq@gmail.com>
2026-08-09 16:48:01 +0900

    xselをbrewからaptに戻す
    vscode-neovim経由のnvimなどデスクトップランチャー起動プロセスはPATHに
    linuxbrewを含まずbrew版xselが解決できずクリップボード連携が壊れるため

--- run_once_01_install_apt.sh
+++ run_once_01_install_apt.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# xsel は brew ではなく必ず apt で入れること。
+# VSCode などデスクトップランチャー経由で起動するプロセス(vscode-neovim の
+# 内部nvimなど)は ~/.zshrc の `brew shellenv` を経由しないため PATH に
+# linuxbrew (/home/linuxbrew/.linuxbrew/bin) が含まれず、brew版xselだと
+# クリップボード連携(:set clipboard+=unnamed)が "xsel is not executable" で
+# 壊れる。apt版は /usr/bin/xsel に入りどのプロセスからも解決できる。
 sudo apt install -y \
   build-essential \
   unzip \
   vlc \
   nautilus-dropbox \
-  mcomix
+  mcomix \
+  xsel
 
